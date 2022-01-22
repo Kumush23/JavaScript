2d1fcbf246dad5f88a970d8cfcbc307e5d7d208d
unknown <kumushhon.rustamova@gmail.com>
2022-01-22 16:00:17 +0500

    added changes

--- Slider2/script.js
+++ Slider2/script.js
@@ -1,20 +1,6 @@
-
-function updateTime() {
-    const sliderItem = document.querySelectorAll('.slider-item') = new Date().toTimeString();
-    for (let i = 0; i < sliderItem.length; i++) {
-        const item = sliderItem[i];
-        if (item.classList.contains('active')) {
-            item.classList.remove('active')
-            const indexItem = sliderItem.length - 1 === i ? 0 : i + 1;
-            sliderItem[indexItem].classList.add('active')
-           break;     
-        }   
-        setInterval(updateTime, 2000);   
-    }
-}
-
-
-
+// setInterval(() => {
+//     document.querySelector('.slider-next').click();
+// }, 2000);
 
 document.querySelector('.slider-next').addEventListener('click', function () {
     const sliderItem = document.querySelectorAll('.slider-item');
@@ -24,8 +10,8 @@ document.querySelector('.slider-next').addEventListener('click', function () {
             item.classList.remove('active')
             const indexItem = sliderItem.length - 1 === i ? 0 : i + 1;
             sliderItem[indexItem].classList.add('active')
-           break;     
-        }      
+            break;
+        }
     }
 })
 
@@ -42,3 +28,34 @@ document.querySelector('.slider-prev').addEventListener('click', function () {
 
     }
 })
+
+document.querySelector('.slide1').addEventListener('click', function (e) {
+    const sliderItem = document.querySelectorAll('.slider-item');
+    for (let i = 0; i < sliderItem.length; i++) {
+        sliderItem[i].classList.remove('active');
+    }
+    const id = e.target.attributes['data-indicator-id'].value
+    sliderItem[id].classList.add('active');
+}
+)
+
+
+document.querySelector('.slide2').addEventListener('click', function (e) {
+    const sliderItem = document.querySelectorAll('.slider-item');
+    for (let i = 0; i < sliderItem.length; i++) {
+        sliderItem[i].classList.remove('active');
+    }
+    const id = e.target.attributes['data-indicator-id'].value
+    sliderItem[id].classList.add('active');
+}
+)
+
+document.querySelector('.slide3').addEventListener('click', function (e) {
+    const sliderItem = document.querySelectorAll('.slider-item');
+    for (let i = 0; i < sliderItem.length; i++) {
+        sliderItem[i].classList.remove('active');
+    }
+    const id = e.target.attributes['data-indicator-id'].value
+    sliderItem[id].classList.add('active');
+}
+)
